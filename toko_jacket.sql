16d458f4d2d25631b6583973f85875ba46388187
Moch Reza <hikmalkoko3@gmail.com>
2023-01-21 22:29:51 +0700

    a

--- toko_jacket.sql
+++ toko_jacket.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2022 at 07:46 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 7.4.30
+-- Generation Time: Jan 21, 2023 at 04:28 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -46,11 +46,11 @@ CREATE TABLE `tbl_barang` (
 --
 
 INSERT INTO `tbl_barang` (`id_barang`, `nama_barang`, `harga`, `total_stok`, `stok_s`, `stok_l`, `stok_m`, `stok_xl`, `deskripsi`, `gambar`, `berat`) VALUES
-(23, 'Hoodie', 700, 8, 2, 2, 2, 2, 'xxxxx xxxx xxxx x x x x xxxx', '1nofoto.jpg', 700),
+(23, 'Hoodie', 700, 7, 1, 2, 2, 2, 'xxxxx xxxx xxxx x x x x xxxx', '1nofoto.jpg', 700),
 (24, 'Hoodie ', 700, 8, 2, 2, 2, 2, 'xxxxxxxx\r\nxxxxxxxxxxxx xxxxx xxxxxxxxxxxx', '1nofoto1.jpg', 200),
 (25, 'Hoodie', 700, 9, 2, 2, 2, 3, 'xxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxxxxxxxxxxxxx xx x x x xxxxxx x x', '1nofoto2.jpg', 300),
 (26, 'Hoodie', 90000, 8, 2, 2, 2, 2, 'XXXXXXXXXX XXXXX XXX XXXXXXXXXXX', '1nofoto3.jpg', 900),
-(27, 'Hoodie', 700, 8, 2, 2, 2, 2, 'xxxxxxxxxxxxxxx xxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxx    xxxxxxxxxxxxx', '1nofoto4.jpg', 500);
+(28, 'jaket brewok', 1234567, 9, 1, 3, 3, 2, 'asdfasdfasdfasdfasdfasdf', 'Picture1.jpg', 123);
 
 -- --------------------------------------------------------
 
@@ -87,7 +87,8 @@ INSERT INTO `tbl_gambar` (`id_gambar`, `id_barang`, `ket`, `gambar`) VALUES
 (33, 6, 'jono', 'topi.png'),
 (34, 14, 'ini adalah keterangan', 'spesifikasi-printer-canon-pixma-mp237.jpg'),
 (35, 13, 'dari saping kanan', 'DSC_8884.JPG'),
-(36, 21, 'keterangan barang', 'kindpng_1340776.png');
+(36, 21, 'keterangan barang', 'kindpng_1340776.png'),
+(37, 28, 'qewrqewrqerqwer', 'Untitled_Diagram-Page-2.png');
 
 -- --------------------------------------------------------
 
@@ -107,19 +108,11 @@ CREATE TABLE `tbl_pelanggan` (
 --
 
 INSERT INTO `tbl_pelanggan` (`id_pelanggan`, `nama_pelanggan`, `email`, `password`) VALUES
-(1, 'san', 'sandyrick85@gmail.com', '12345'),
-(3, 'waskita', 'waswas@gmail.com', '12345'),
-(4, 'dandi', 'pelanggan@gmail.com', '12345'),
-(5, 'ricky', 'ricky@gmail.com', '12345'),
-(6, 'cika', 'cika@gmail.com', 'cika'),
 (8, 'waskita', 'wasktia@gmail.com', 'waskita'),
 (9, 'waskita', 'waskita@gmail.com', 'waskita'),
 (10, 'waskita', 'waskita1234@gmail.com', 'waskita'),
 (12, 'waskita12', 'waskita12@gmail.com', 'waskita'),
-(13, 'nugraha12', 'nugraha12@gmail.com', 'waskita'),
-(14, 'warih ', 'warih@gmail.com', 'warih123'),
-(15, 'waskita 2', 'waskita2@gmail.com', 'waskita1717'),
-(16, 'waskita098', 'waskita098@gmail.com', 'waskita098');
+(13, 'nugraha12', 'nugraha12@gmail.com', 'waskita');
 
 -- --------------------------------------------------------
 
@@ -140,7 +133,8 @@ CREATE TABLE `tbl_rekening` (
 
 INSERT INTO `tbl_rekening` (`id_rekening`, `nama_bank`, `no_rek`, `atas_nama`) VALUES
 (1, ' MANDIRI', ' 9090-0000-9999-9090', ' sandy ricky destiawan'),
-(4, 'PLECIT', '034534099345093490', 'WASKTIA WARIH NUGRAHA');
+(4, 'PLECIT', '034534099345093490', 'WASKTIA WARIH NUGRAHA'),
+(5, 'jateng', 'p5ou2q35434', 'cika');
 
 -- --------------------------------------------------------
 
@@ -155,6 +149,15 @@ CREATE TABLE `tbl_rinci_transaksi` (
   `qty` int(20) NOT NULL,
   `ukuran` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_rinci_transaksi`
+--
+
+INSERT INTO `tbl_rinci_transaksi` (`id_rinci`, `no_order`, `id_barang`, `qty`, `ukuran`) VALUES
+(91, '202301151QZLREKB', 28, 1, 'XL'),
+(92, '20230115FRCSQOHJ', 23, 1, 'S'),
+(93, '20230115QJV1OI7C', 28, 2, 'S');
 
 --
 -- Triggers `tbl_rinci_transaksi`
@@ -186,7 +189,7 @@ CREATE TABLE `tbl_setting` (
 --
 
 INSERT INTO `tbl_setting` (`id`, `nama_toko`, `lokasi`, `alamat_toko`, `no_telpon`) VALUES
-(1, 'Black Market', 80351, 'jalan palagan km 90', '081226931079');
+(1, 'Black Market', 33719, 'jalan palagan km 90', '081226931079');
 
 -- --------------------------------------------------------
 
@@ -226,7 +229,6 @@ CREATE TABLE `tbl_transaksi` (
 --
 
 INSERT INTO `tbl_transaksi` (`id_transaksi`, `id_pelanggan`, `no_order`, `tgl_order`, `nama_penerima`, `hp_penerima`, `provinsi`, `kota`, `alamat`, `kode_pos`, `expedisi`, `paket`, `estimasi`, `ongkir`, `berat`, `grand_total`, `total_bayar`, `status_bayar`, `bukti_bayar`, `atas_nama`, `nama_bank`, `no_rek`, `status_order`, `no_resi`) VALUES
-(44, 5, '202206232AFTL8IU', '2022-06-23', 'sandy ricky', '127', '18', 'Tulang Bawang', 'jl.pilangsari, -', '57486', 'jne', 'OKE', '3-6 Hari', 41000, 500, 35000, 76000, 1, 'bukti4.jpg', 'Sandyyy', 'BCA', '9090-0000-9999-9090', 3, 'KLT0989800'),
 (46, 9, '20221122PV9S4D8W', '2022-11-22', 'was', '127', '19', 'Buru', 'rtwtergefgf', '55582', 'tiki', 'REG', '5 Hari', 158000, 7, 3213213, 3371213, 1, 'Tugas_Inggris.jpg', 'wasd', 'BRI', '6346546', 3, '67576585786'),
 (47, 10, '20221126M736RLEB', '2022-11-26', 'nugraha', '127', '18', 'Bandar Lampung', 'rtwtergefgf', '55582', 'jne', 'REG', '1-2 Hari', 99000, 3000, 3000000, 3099000, 1, 'IMG_4101.JPG', 'wasd', 'BRI', '6346546', 3, '67576585786'),
 (48, 10, '20221126XU6XSFKQ', '2022-11-26', 'nugraha', '127', '5', 'Yogyakarta', 'rtwtergefgf', '55582', 'jne', 'YES', '1-1 Hari', 294000, 21000, 540000, 834000, 1, 'DSC_8884.JPG', 'nugraha', 'BCA', '43523452', 3, '67576585786'),
@@ -236,10 +238,7 @@ INSERT INTO `tbl_transaksi` (`id_transaksi`, `id_pelanggan`, `no_order`, `tgl_or
 (55, 12, '20221127JRZUWKAD', '2022-11-27', 'waskita12', '127', '1', '80351', 'rtwtergefgf', '55582', 'jne', 'REG', '3-4 Hari', 34000, 192, 150000, 184000, 1, 'Screenshot_(27)1.png', 'wasd', 'BRI', '034534099345093490', 3, '341234123412341234'),
 (56, 12, '20221127XVQK36WY', '2022-11-27', 'waskita12', '123456789123', '1', '80351', 'sleman ', '55582', 'jne', 'OKE', '7-8 Hari', 64000, 2000, 2000000, 2064000, 1, 'Screenshot_(14).png', 'cika', 'PLECIT', '034534099345093490', 3, '4321'),
 (57, 12, '20221127YHLVOO6I', '2022-11-27', 'waskita12', '123436346456', '1', '80351', 'sleman ', '55582', 'jne', 'OKE', '7-8 Hari', 32000, 64, 50000, 82000, 0, NULL, NULL, NULL, NULL, 0, NULL),
-(58, 12, '20221127IHKUQ1HF', '2022-11-27', 'waskita12', '123412341234', '3', '42417', 'pakem', '55582', 'jne', 'OKE', '4-5 Hari', 196000, 14000, 360000, 556000, 0, NULL, NULL, NULL, NULL, 0, NULL),
-(59, 14, '20221129UIQW4UFN', '2022-11-29', 'warih', '081226931073', '22', '84315', 'pakem', '55582', 'pos', 'Pos Reguler', '5 HARI Hari', 46000, 800, 14000, 60000, 1, 'meja-komputer-kantor-minimalis-299x203.jpg', 'warih', 'jateng', '123987451234', 3, '341234123412341234'),
-(60, 15, '202212093PWGIJBD', '2022-12-09', 'waskita2', '081226931073', '1', '80351', 'sleman ', '55582', 'jne', 'OKE', '4-5 Hari', 48000, 400, 100000, 148000, 1, 'WhatsApp_Image_2022-10-17_at_18_55_12.jpeg', 'waskita 2', 'BRI', 'p5ou2q35434', 3, '341234123412341234'),
-(61, 16, '20221222ZAA2TNTO', '2022-12-22', 'waskita', '081226931073', '1', '80351', 'pakem sleman yk', '90909', 'jne', 'CTC', '1-1 Hari', 16000, 1600, 2800, 18800, 1, 'Ge-hUAWZ_400x400.jpg', 'waskita', 'BRI', '034534099345093490', 3, '6585875857');
+(58, 12, '20221127IHKUQ1HF', '2022-11-27', 'waskita12', '123412341234', '3', '42417', 'pakem', '55582', 'jne', 'OKE', '4-5 Hari', 196000, 14000, 360000, 556000, 0, NULL, NULL, NULL, NULL, 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -260,7 +259,6 @@ CREATE TABLE `tbl_user` (
 --
 
 INSERT INTO `tbl_user` (`id_user`, `nama_user`, `username`, `password`, `level_user`) VALUES
-(1, ' mardial', ' admin', ' admin', 1),
 (6, 'waskita', 'waskita', 'waskita', 1),
 (1234, 'was', 'was', 'was', 1),
 (1236, 'warih', 'warih', 'warih', 1);
@@ -330,31 +328,31 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `tbl_barang`
 --
 ALTER TABLE `tbl_barang`
-  MODIFY `id_barang` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id_barang` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `tbl_gambar`
 --
 ALTER TABLE `tbl_gambar`
-  MODIFY `id_gambar` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id_gambar` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `tbl_pelanggan`
 --
 ALTER TABLE `tbl_pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `tbl_rekening`
 --
 ALTER TABLE `tbl_rekening`
-  MODIFY `id_rekening` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_rekening` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tbl_rinci_transaksi`
 --
 ALTER TABLE `tbl_rinci_transaksi`
-  MODIFY `id_rinci` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+  MODIFY `id_rinci` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 
 --
 -- AUTO_INCREMENT for table `tbl_setting`
@@ -366,13 +364,13 @@ ALTER TABLE `tbl_setting`
 -- AUTO_INCREMENT for table `tbl_transaksi`
 --
 ALTER TABLE `tbl_transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id_user` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1237;
+  MODIFY `id_user` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1238;
 
 --
 -- Constraints for dumped tables
